d96298c6b7409c2078d59af103a7054d0836f7db
stephen-bradshaw <stephen.bradshaw@thomsonreuters.com>
2012-10-15 12:14:38 +0100

    Add tests back to doh runner

--- src/js/rishson/tests/doh/Tests.js
+++ src/js/rishson/tests/doh/Tests.js
@@ -1,6 +1,6 @@
 define([
 	"doh", // for registerUrl
-/*	"require", // for context-sensitive require and toUrl
+	"require", // for context-sensitive require and toUrl
 	"rishson/tests/doh/control/TestDispatcher",
 	"rishson/tests/doh/control/TestLoginResponse",
 	"rishson/tests/doh/control/TestMockTransport",
@@ -8,8 +8,8 @@ define([
 	"rishson/tests/doh/control/TestResponse",
 	"rishson/tests/doh/control/TestRestRequest",
 	"rishson/tests/doh/control/TestServiceRequest",
-	"rishson/tests/doh/util/TestObjectValidator",*/
+	"rishson/tests/doh/util/TestObjectValidator",
 	"rishson/tests/doh/base/router/RouteParser"
 ], function (doh, require) {
-	//doh.registerUrl("AppContainer tests", require.toUrl("./view/TestAppContainer.html"), 999999);
+	doh.registerUrl("AppContainer tests", require.toUrl("./view/TestAppContainer.html"), 999999);
 });
